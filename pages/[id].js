40fb72ed8b8f00c85dee3f352cd753e8ea544f4b
datejer <ejer.priv@wp.pl>
2021-02-07 22:45:18 +0100

    Fix loader

--- pages/[id].js
+++ pages/[id].js
@@ -15,7 +15,7 @@ const Paste = () => {
 				.get(`/api/paste?id=${router.query.id}`)
 				.then((response) => {
 					setPaste(response.data);
-					setTimeout(setLoading(false), 5000);
+					setLoading(false);
 				})
 				.catch((error) => {
 					router.push("/");
@@ -29,25 +29,21 @@ const Paste = () => {
 
 			<main className={styles.main}>
 				<h1 className={styles.title}>{router.query.id}</h1>
-				<code className={styles.code}>
-					{loading ? (
-						<div className="lds-ring">
-							<div></div>
-							<div></div>
-							<div></div>
-							<div></div>
-						</div>
-					) : (
-						paste.content.split(/\n/).map((item, key) => {
+
+				{loading ? (
+					<div className={styles.dotflashing}></div>
+				) : (
+					<code className={styles.code}>
+						{paste.content.split(/\n/).map((item, key) => {
 							return (
 								<span key={key}>
 									{item}
 									<br />
 								</span>
 							);
-						})
-					)}
-				</code>
+						})}
+					</code>
+				)}
 			</main>
 
 			<footer className={styles.footer}>
