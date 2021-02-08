2e38082f727737665fc2628c55a38d1e32d36648
datejer <ejer.priv@wp.pl>
2021-02-08 14:58:34 +0100

    hotfix :fire: Layout takes children

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,6 +1,6 @@
 import styles from "./Layout.module.scss";
 
-export default function Layout() {
+export default function Layout({ children }) {
 	return (
 		<div className={styles.container}>
 			<main className={styles.main}>{children}</main>
