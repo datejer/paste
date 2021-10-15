2a72b2def98a261bed6042c8942549bec69abf43
datejer <ejer.priv@wp.pl>
2021-10-15 11:22:06 +0200

    Fixes

--- pages/api/paste/raw.js
+++ pages/api/paste/raw.js
@@ -1,6 +1,7 @@
 import database from '../../../middlewares/database';
 import nextConnect from 'next-connect';
 import { ObjectID } from 'mongodb';
+import shortid from 'shortid';
 
 let tagsToReplace = {
 	'&': '&amp;',
@@ -24,32 +25,47 @@ handler.get(async (req, res) => {
 	if (!req.query.id || req.query.id.length <= 0)
 		return res.status(404).json({ message: 'Please provide an ID!' });
 
-	req.db
-		.collection('pastes')
-		.findOne({
-			$or: [
-				{
-					_id: new ObjectID(req.query.id),
-				},
-				{
-					id: req.query.id,
-				},
-			],
-		})
-		.then(paste => {
-			if (!paste)
-				return res
-					.status(404)
-					.json({ message: "Couldn't find a paste with this ID!" });
+	if (shortid.isValid(req.query.id)) {
+		req.db
+			.collection('pastes')
+			.findOne({
+				id: req.query.id,
+			})
+			.then(paste => {
+				if (!paste)
+					return res
+						.status(404)
+						.json({ message: "Couldn't find a paste with this ID!" });
 
-			return res
-				.status(200)
-				.send(
-					`<pre style="word-wrap: break-word; white-space: pre-wrap;">${escapeHTML(
-						paste.content
-					)}</pre>`
-				);
-		});
+				return res
+					.status(200)
+					.send(
+						`<pre style="word-wrap: break-word; white-space: pre-wrap;">${escapeHTML(
+							paste.content
+						)}</pre>`
+					);
+			});
+	} else {
+		req.db
+			.collection('pastes')
+			.findOne({
+				_id: new ObjectID(req.query.id),
+			})
+			.then(paste => {
+				if (!paste)
+					return res
+						.status(404)
+						.json({ message: "Couldn't find a paste with this ID!" });
+
+				return res
+					.status(200)
+					.send(
+						`<pre style="word-wrap: break-word; white-space: pre-wrap;">${escapeHTML(
+							paste.content
+						)}</pre>`
+					);
+			});
+	}
 });
 
 export default handler;
